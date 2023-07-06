9f66f6f0e6067f7e847453439e42fc72a4d1a50f
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-07-06 10:39:32 +0200

    urls too

--- robot_ui/robot_ui/urls.py
+++ robot_ui/robot_ui/urls.py
@@ -1,13 +1,13 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from .schema import schema
-from .admin import my_admin_site
 
 
 urlpatterns = [
-    path('', my_admin_site.urls),
+    path('', admin.site.urls),
     # path('', include('data.urls')),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)), name='graphql'),
 ]
