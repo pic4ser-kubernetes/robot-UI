683df965d03ded33d5d5ea514353649aa8008354
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-08-25 16:48:36 +0200

    using daphne as web server

--- robot_ui/entrypoint.sh
+++ robot_ui/entrypoint.sh
@@ -1,4 +1,5 @@
 python manage.py migrate &
 python manage.py collectstatic --noinput &
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
 # gunicorn robot_ui.wsgi --bind 0.0.0.0:8000 --workers=3
+daphne -b 0.0.0.0 -p 8000 robot_ui.asgi:application
