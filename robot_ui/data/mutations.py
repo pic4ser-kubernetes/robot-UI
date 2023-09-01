1e4205b91408d49b52734f6c006377fa3f2c8d03
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-09-01 12:18:47 +0200

    error handling in mutations

--- robot_ui/data/mutations.py
+++ robot_ui/data/mutations.py
@@ -12,20 +12,23 @@ class UpdateStatusMutation(graphene.Mutation):
 
     @classmethod
     def mutate(cls, root, info, status_dict: StatusInput):
-        robot = RobotSession.objects.get(
-            name=status_dict.robot,
-            session__name=status_dict.session,
-            session__active=True
-        )
+        try:
+            robot = RobotSession.objects.get(
+                name=status_dict.robot,
+                session__name=status_dict.session,
+                session__active=True
+            )
 
-        RobotStatus.objects.create(
-            robot=robot,
-            name=status_dict.name,
-            status=status_dict.status,
-            timestamp=status_dict.timestamp
-        )
-
-        return cls(ok=True)
+            RobotStatus.objects.create(
+                robot=robot,
+                name=status_dict.name,
+                status=status_dict.status,
+                timestamp=status_dict.timestamp
+            )
+        except Exception as e:
+            return cls(ok=False, error=str(e))
+        else:
+            return cls(ok=True)
 
 
 class AddDataMutation(graphene.Mutation):
@@ -33,25 +36,27 @@ class AddDataMutation(graphene.Mutation):
         data_dict = DataInput(required=True)
 
     ok = graphene.Boolean(required=True)
+    error = graphene.String()
 
     @classmethod
     def mutate(cls, root, info, data_dict: DataInput):
-        data_group = DataGroup.objects.get(
-            name=data_dict.data_group,
-            robot_session__name=data_dict.robot,
-            robot_session__session__name=data_dict.session,
-            robot_session__session__active=True
-        )
+        try:
+            data_group = DataGroup.objects.get(
+                name=data_dict.data_group,
+                robot_session__name=data_dict.robot,
+                robot_session__session__name=data_dict.session,
+                robot_session__session__active=True
+            )
 
-        RobotData.objects.create(
-            data_group=data_group,
-            data=data_dict.data,
-            timestamp=data_dict.timestamp,
-        )
-
-        return cls(ok=True)
-
-# TODO: handle errors
+            RobotData.objects.create(
+                data_group=data_group,
+                data=data_dict.data,
+                timestamp=data_dict.timestamp,
+            )
+        except Exception as e:
+            return cls(ok=False, error=str(e))
+        else:
+            return cls(ok=True)
 
 
 class AddWebcamMutation(graphene.Mutation):
@@ -62,21 +67,24 @@ class AddWebcamMutation(graphene.Mutation):
 
     @classmethod
     def mutate(cls, root, info, webcam_dict: WebcamInput):
-        robot = RobotSession.objects.get(
-            name=webcam_dict.robot,
-            session__name=webcam_dict.session,
-            session__active=True
-        )
+        try:
+            robot = RobotSession.objects.get(
+                name=webcam_dict.robot,
+                session__name=webcam_dict.session,
+                session__active=True
+            )
 
-        RobotWebcam.objects.update_or_create(
-            robot=robot,
-            name=webcam_dict.name,
-            defaults={
-                'url': webcam_dict.url
-            }
-        )
-
-        return cls(ok=True)
+            RobotWebcam.objects.update_or_create(
+                robot=robot,
+                name=webcam_dict.name,
+                defaults={
+                    'url': webcam_dict.url
+                }
+            )
+        except Exception as e:
+            return cls(ok=False, error=str(e))
+        else:
+            return cls(ok=True)
 
 
 class Mutation(graphene.ObjectType):
